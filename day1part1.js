6a649eb919b61ae0c017fb83dc031ed7a2263d65
AK <captainbuckwheat@gmail.com>
2016-03-22 17:03:55 -0700

    Update day1part1.js

--- day1part1.js
+++ day1part1.js
@@ -1,4 +1,4 @@
-/*
+/* REFER TO day1_data.js for input
  Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! 
  To save Christmas, he needs you to collect fifty stars by December 25th.
  
