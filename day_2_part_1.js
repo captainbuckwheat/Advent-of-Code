e1c7eb3623d87b0a7dfc2c1b4ba33b4aa8621628
AK <captainbuckwheat@gmail.com>
2016-04-28 15:15:19 -0700

    Update and rename day2part1.js to day_2_part_1.js

--- day_2_part_1.js
+++ day_2_part_1.js
@@ -1,4 +1,4 @@
-/* REFER TO day2_data.js for input 
+/* REFER TO day_2_data.js for input 
 The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
 Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
 For example:
@@ -7,17 +7,18 @@ A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of 
 All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
 */
 
-var min = function(a,b,c){
+var min = function(a,b,c) {
   var z = a; 
-  if (b < z){
+  if (b < z) {
     z = b;
-  } else if (c < z){
+  } else if (c < z) {
     z = c;
   }
   console.log(z);
   return(z);
 }
-var wrapping_area = function(l,w,h){
+
+var wrapping_area = function(l,w,h) {
   var a, b, c;
   a = 2 * l * w; 
   b = 2 * w * h;
@@ -25,12 +26,13 @@ var wrapping_area = function(l,w,h){
   console.log('c is '+c);
   return (a + b + c + min(a,b,c)/2);
 }
-var total_paper = function(q){
+
+var total_paper = function(q) {
   var x, sum, i, boxes, each; 
   sum = 0; 
   i = 0; 
   boxes = q.split(' ');
-  while (i < boxes.length){
+  while (i < boxes.length) {
     each = boxes[i].split('x');
     x = wrapping_area(each[0], each[1], each[2]);
     sum = sum + x; 
@@ -39,8 +41,8 @@ var total_paper = function(q){
   return(sum);
 }
 
-var test = function(){
-  if (total_paper('2x3x4 1x1x10')!==101){
+var test = function() {
+  if (total_paper('2x3x4 1x1x10')!==101)  {
     console.log('test has failed');
   } else {
     console.log('it works like a fkn clock!')
