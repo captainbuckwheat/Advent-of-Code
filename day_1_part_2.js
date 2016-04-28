19fbcf765c2a405c16336f65ce3719f2ab723097
AK <captainbuckwheat@gmail.com>
2016-04-28 15:16:28 -0700

    Update and rename day1part2.js to day_1_part_2.js

--- day_1_part_2.js
+++ day_1_part_2.js
@@ -1,4 +1,4 @@
-/* REFER TO day1_data.js for input
+/* REFER TO day_1_data.js for input
 Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
 For example:
 - ) causes him to enter the basement at character position 1.
