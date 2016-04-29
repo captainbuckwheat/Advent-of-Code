6dce7c23d9804dc2b8fe7f08d0a4a9f7498039b6
AK <captainbuckwheat@gmail.com>
2016-04-28 17:57:45 -0700

    Update day_2_part_2.js

--- day_2_part_2.js
+++ day_2_part_2.js
@@ -1,4 +1,4 @@
-/* REFER TO day_2_data.js for the input 
+/* REFER TO day_2_data.js TO GET THE INPUT
 The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
 The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
 For example:
@@ -7,8 +7,9 @@ A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the
 How many total feet of ribbon should they order?
 */
 
-var min = function(a,b,c){
-  var z = a; 
+var min = function(a, b, c) {
+  var z; 
+  z = a; 
   if (b < z) {
     z = b;
   }
@@ -18,8 +19,8 @@ var min = function(a,b,c){
   return z; 
 }
 
-ribbon_bow = function(l,w,h){
-  var a,b,c, ribbon, volume; 
+ribbon_bow = function(l, w, h) {
+  var a, b, c, ribbon, volume, bow; 
   a = 2 * l + 2 * w; 
   b = 2 * w + 2 * h; 
   c = 2 * h + 2 * l; 
@@ -28,8 +29,8 @@ ribbon_bow = function(l,w,h){
   return ribbon+bow; 
 }
 
-total_ribbon_bow = fucntion(q) {
-  var x, sum, i, boxes, each: 
+total_ribbon_bow = function(q) {
+  var x, sum, i, boxes, each; 
   sum = 0; 
   i = 0; 
   boxes = q.split(" "); 
