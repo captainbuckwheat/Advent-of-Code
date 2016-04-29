dcf796cbab38e26ab13eb38c49775eac530463fd
AK <captainbuckwheat@gmail.com>
2016-04-28 17:52:57 -0700

    Update day_1_part_2.js

--- day_1_part_2.js
+++ day_1_part_2.js
@@ -1,4 +1,4 @@
-/* REFER TO day_1_data.js for input
+/* REFER TO day_1_data.js TO GET THE INPUT
 Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
 For example:
 - ) causes him to enter the basement at character position 1.
@@ -6,29 +6,29 @@ For example:
 What is the position of the character that causes Santa to first enter the basement?
 */
 
-var basement = function(x){
+var basement = function(x) {
   var x, sum, i; 
   var sum = 0; 
   var i = 0; 
-  while (i < x.length){
-    if (x[i] === "("){
+  while (i < x.length) {
+    if (x[i] === "(") {
       sum = sum + 1; 
-    } else if (x[i] === ")"){
+    } else if (x[i] === ")") {
       sum = sum - 1;
     };
-    if (sum === - 1){
-      return(i+1);
+    if (sum === - 1) {
+      return i+1;
     } else {
       i = i + 1;
     }
   }
-  return(sum); 
+  return sum; 
 }; 
 
-var test = function(){
-  if (basement("()())")!==5){
+var test = function() {
+  if (basement("()())") !==5 ) {
     console.log('test has failed #1');
-  } else if (basement(")")!==1){
+  } else if (basement(")") !==1 ){
     console.log('test has failed #2');
   } else {
     console.log('it works like a fkn clock!')
